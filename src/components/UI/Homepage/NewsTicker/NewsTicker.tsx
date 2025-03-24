5b1a98c46b34e79ec456f36594721e0c43785341
abdullah-mamun369 <maamamun369@gmail.com>
2025-03-25 04:02:56 +0600

    Md version of team partner is completed

--- src/components/UI/Homepage/NewsTicker/NewsTicker.tsx
+++ src/components/UI/Homepage/NewsTicker/NewsTicker.tsx
@@ -4,7 +4,7 @@ const NewsTicker = () => {
   const floatingText = "LOREM IPSUM IS SIMPLY DUMMY TEXT OF THE PRINTING AND";
 
   return (
-    <div className="absolute hidden md:block bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 z-20 bg-primary text-white text-2xl font-semibold p-9 border border-secondary shadow-lg whitespace-nowrap overflow-hidden max-w-7xl">
+    <div className="absolute hidden md:block bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 z-20 bg-primary text-white text-2xl font-semibold p-9 border border-secondary shadow-xl whitespace-nowrap overflow-hidden max-w-7xl">
       <div>
         <div className={`${styles.floatingText}`}>{floatingText.repeat(2)}</div>
       </div>
@@ -13,12 +13,3 @@ const NewsTicker = () => {
 };
 
 export default NewsTicker;
-
-{
-  /* Floating Extra Text Section Slider
-   <div className="absolute bottom-0 left-0 w-full z-20 bg-[#0a2742] text-white text-[18px] md:text-[24px] font-semibold py-2 border-t border-white shadow-lg overflow-hidden">
-   <div className="animate-continuous-slide whitespace-nowrap">
-     {floatingText.repeat(10)}
-   </div>
-  </div> */
-}
--- src/components/UI/Homepage/TeamPartners/TeamPartners.tsx
+++ src/components/UI/Homepage/TeamPartners/TeamPartners.tsx
@@ -14,13 +14,13 @@ import tahmina from "@/assets/Tahmina 1.png";
 
 const TeamPartners = () => {
   return (
-    <Container className="mt-10 lg:mt-32">
+    <Container className="mt-10 lg:mt-32 mb-10 lg:mb-20">
       <Heading
         title="TEAM PARTNERS"
         subtitle="Meet our partners who are supporting us for 2025 season."
       />
       <div className=" grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-0 mt-6 md:mt-6">
-        <div className="relative border-gray-300 mr-4">
+        <div className="relative border-gray-300 mr-4 ">
           <div className="border-t-2 border-b-2 border-gray-300 mr-5">
             <div>
               <h3 className="text-primary text-2xl font-semibold text-center mt-5">
@@ -45,9 +45,9 @@ const TeamPartners = () => {
               </div>
             </div>
           </div>
-          <div className="absolute top-[20px] right-0 h-[calc(100%-40px)] w-[2px] bg-[#E5E5E5]"></div>
+          <div className="hidden md:block absolute top-[20px] right-0 h-[calc(100%-40px)] w-[2px] bg-[#E5E5E5]"></div>
         </div>
-        <div className="border-gray-300 ">
+        <div className="border-gray-300 hidden md:block">
           <div className="border-t-2 border-b-2 border-gray-300 mr-5">
             <div>
               <h3 className="text-primary text-2xl font-semibold text-center mt-5">
@@ -73,11 +73,11 @@ const TeamPartners = () => {
             </div>
           </div>
         </div>
-        <div className="relative border-gray-300 mr-4">
+        <div className="relative border-gray-300 mr-4 hidden md:block">
           <div className=" border-b-2 border-gray-300 mr-5">
             <div>
-              <h3 className="text-primary text-2xl font-semibold text-center mt-5">
-                PRINCIPAL PARTNERS
+              <h3 className="text-primary text-2xl font-semibold text-center mt-5 uppercase">
+                Associate Sponsor
               </h3>
 
               <div className="flex justify-center items-center gap-4 md:gap-8 mt-4 md:mt-8 mb-10">
@@ -100,11 +100,11 @@ const TeamPartners = () => {
           </div>
           <div className="absolute top-[20px] right-0 h-[calc(100%-40px)] w-[2px] bg-[#E5E5E5]"></div>
         </div>
-        <div className="border-gray-300 ">
+        <div className="border-gray-300 hidden md:block">
           <div className="border-b-2 border-gray-300 mr-5">
             <div>
-              <h3 className="text-primary text-2xl font-semibold text-center mt-5">
-                PRINCIPAL PARTNERS
+              <h3 className="text-primary text-2xl font-semibold text-center mt-5 uppercase">
+                Associate Sponsor
               </h3>
 
               <div className="flex justify-center items-center gap-4 md:gap-8 mt-4 md:mt-8 mb-10">
