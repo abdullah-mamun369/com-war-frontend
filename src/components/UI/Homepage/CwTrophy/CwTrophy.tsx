d132beef146a2a2d60360d92d3f6ec2a99523333
abdullah-mamun369 <maamamun369@gmail.com>
2025-04-10 03:12:27 +0600

    basic cw trophy section is done

--- src/components/UI/Homepage/CwTrophy/CwTrophy.tsx
+++ src/components/UI/Homepage/CwTrophy/CwTrophy.tsx
@@ -7,23 +7,22 @@ import Button from "../../Button/Button";
 
 const CwTrophy = () => {
   return (
-    <section>
+    <section className="relative w-full">
       <Image
         src={cwTrophyBg}
         alt="C.W. Trophy"
         width={1920}
         height={100}
-        className="w-full relative"
+        className="w-full"
       />
-      <Container className="absolute flex justify-center items-center z-50">
-        <Heading
-          title="C.W. SHOP"
-          subtitle="Get your hands on exclusive Comilla Warriors gear and merchandise."
-        />
-        <div className="mt-3 md:mt-10 text-center">
-          <Button variant="contained" color="primary">
-            VIEW SHOP
-          </Button>
+      <Container className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+        <div className="text-center px-5 lg:px-0">
+          <h1 className="text-2xl md:text-4xl font-bold text-white">
+            <span className="text-accent">C.W.</span> SHOP
+          </h1>
+          <p className="text-base text-white">
+            Get your hands on exclusive Comilla Warriors gear and merchandise.
+          </p>
         </div>
       </Container>
     </section>
