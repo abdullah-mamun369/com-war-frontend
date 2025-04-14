580504725775aadfa04fab4a0a91a9f38e99ed20
abdullah-mamun369 <maamamun369@gmail.com>
2025-04-14 15:18:01 +0600

    Loader is added to the Gallery

--- src/components/UI/Homepage/Gallery/Gallery.tsx
+++ src/components/UI/Homepage/Gallery/Gallery.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import Button from "../../Button/Button";
 import Container from "../../Container/Container";
 import Heading from "../../Heading/Heading";
 import ImageContainer from "./ImageContainer";
+import { useEffect, useState } from "react";
+import LoadingDesign from "../../LoadingDesign/LoadingDesign";
 
 const Gallery = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate data fetching delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500); // Adjust time if needed
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <section className="mb-20">
       <Container className=" flex flex-col items-center justify-center h-full px-4">
@@ -13,7 +27,13 @@ const Gallery = () => {
           subtitle="Explore all the unforgettable moments with Comilla Warriors."
         />
         <div>
-          <ImageContainer />
+          {loading ? (
+            <div className="lg:h-96 lg:w-96 flex justify-center items-center">
+              <LoadingDesign />
+            </div>
+          ) : (
+            <ImageContainer />
+          )}
         </div>
 
         <div className="mt-3 md:mt-10 text-center">
