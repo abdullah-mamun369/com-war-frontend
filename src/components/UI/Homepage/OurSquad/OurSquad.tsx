f5fa96756cff06c2b202076ef822ebbaf934a8a4
abdullah-mamun369 <maamamun369@gmail.com>
2025-04-14 15:10:54 +0600

    Loader is added to the Our squad

--- src/components/UI/Homepage/OurSquad/OurSquad.tsx
+++ src/components/UI/Homepage/OurSquad/OurSquad.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import footerBg from "@/assets/squad-bg.png";
 import Container from "../../Container/Container";
@@ -5,8 +7,20 @@ import Button from "../../Button/Button";
 import Heading from "../../Heading/Heading";
 import Players from "./Players";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import LoadingDesign from "../../LoadingDesign/LoadingDesign";
 
 const OurSquad = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate data fetching delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500); // Adjust time if needed
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <section className="relative hidden md:flex justify-center items-center w-full h-[500px] sm:h-[600px] md:h-[700px] bg-cover bg-center mb-20">
       <div className="absolute -z-10 w-full">
@@ -18,19 +32,21 @@ const OurSquad = () => {
           height={675}
         />
       </div>
-      {/* Content on top of the background */}
-      <Container className=" flex flex-col items-center justify-center h-full text-white text-center px-4">
+
+      <Container className="flex flex-col items-center justify-center h-full text-white text-center px-4">
         <Heading title="OUR SQUAD" subtitle="Meet our squad for 2025 season." />
         <div className="my-6 md:my-8">
-          <Players />
+          {loading ? <LoadingDesign /> : <Players />}
         </div>
-        <div className="mt-3 md:mt-6 text-center">
-          <Link href={"/team"}>
-            <Button variant="contained" color="primary">
-              VIEW FULL SQUAD
-            </Button>
-          </Link>
-        </div>
+        {!loading && (
+          <div className="mt-3 md:mt-6 text-center">
+            <Link href={"/team"}>
+              <Button variant="contained" color="primary">
+                VIEW FULL SQUAD
+              </Button>
+            </Link>
+          </div>
+        )}
       </Container>
     </section>
   );
